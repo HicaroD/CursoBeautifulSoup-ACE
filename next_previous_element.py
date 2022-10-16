4daba55293b39d802c0c113bfd1153978df43843
HicaroD <hdanrlley1@gmail.com>
2022-10-16 20:14:28 -0300

    uanderson: atualizando código para novo website

--- next_previous_element.py
+++ next_previous_element.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-url='https://www.populationpyramid.net/pt/popula%C3%A7%C3%A3o/2022/'
+url='https://en.wikipedia.org/wiki/Beautiful_Soup_(HTML_parser)'
 
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-find_a = soup.find_all("a")[0]
+find_a = soup.find_all("a")[4]
 print('-------------------------------------------------------------------------')
 print(find_a)
 print('-------------------------------------------------------------------------')
@@ -15,7 +15,6 @@ print(find_a.next_element)
 print('-------------------------------------------------------------------------')
 #previous_element tras o elemento anterior
 print(find_a.previous_element)
-print('-------------------------------------------------------------------------')
 
 
 
