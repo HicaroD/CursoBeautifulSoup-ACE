fd21f2fb80a92af4dd25bbcba61909852730d93b
HicaroD <hdanrlley1@gmail.com>
2022-10-16 19:55:59 -0300

    hicaro: alterando exemplos

--- navigating_on_html_file.py
+++ navigating_on_html_file.py
@@ -3,8 +3,12 @@ import requests
 
 
 def main():
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    website = "https://en.wikipedia.org/wiki/Beautiful_Soup_(HTML_parser)"
     response = requests.get(
-        "https://www.populationpyramid.net/pt/popula%C3%A7%C3%A3o/2022/"
+        website,
+        headers=headers,
     )
 
     if response.status_code != 200:
@@ -18,26 +22,27 @@ def main():
     soup = BeautifulSoup(html_doc, "html.parser")
 
     """
-    Esse comando abaixo irá buscar a primeira tag <a> dentro do HTML
+    Esse comando abaixo irá buscar a primeira tag <div> dentro do HTML
     """
-    print(soup.a)
+    print(soup.div)
 
     """
-    Esse comando abaixo irá extrair o texto de dentro da primeira tag <a> dentro do HTML
+    Esse comando abaixo irá extrair o texto de dentro da primeira tag <p> dentro do HTML
     """
-    print(soup.a.text)
+    print(soup.p.text)
 
     """
     Esse comando irá extrair todas as children da primeira tag <div> que aparecer dentro
     do HTML.
     """
-    for child in soup.div.children:
+    for child in soup.p.children:
         print(child)
 
     """
     Esse comando irá extrair todos os parent da primeira tag <div> que aparecer dentro
     do HTML.
     """
+    print(soup.div)
     for parent in soup.div.parents:
         print(parent.name)
 
